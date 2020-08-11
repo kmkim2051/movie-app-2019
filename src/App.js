42f306f1e4049d429912c7a5432b7f847ed53466
Kim KyungMim <qaws2051@gmail.com>
2020-08-11 17:25:35 +0900

    apply map to Food Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import Potato from "./Potato";
+
+
+function Food({ name, url}) {
+  return (
+    <div>
+      <h3>Hello {name}</h3>
+      <h3>Go to <a href={url}>{name}</a> </h3>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "kimchi",
+    url: "www.kimchi.com"
+  },
+  {
+    id: 2,
+    name: "ramen",
+    url: "www.ramen.com"
+  }  
+];
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Potato />
+      {foodILike.map(dish => (
+          <Food key={dish.id} name={dish.name} url={dish.url}/>
+      ))}
     </div>
   );
 }
